fe6efd5c33980000873bf094fba5d3d83051fe49
BenjaminAckermans <133055448+BenjaminAckermans@users.noreply.github.com>
2025-09-26 14:49:28 +0200

    Update Final script.py

--- Assignment 4/Final script.py
+++ Assignment 4/Final script.py
@@ -1,14 +1,5 @@
-# -*- coding: utf-8 -*-
 """
-Spyder Editor
+Final script
 
-This is a temporary script file.
+This is the final version of the script that we deliver together with the report.
 """
-<<<<<<< HEAD
-
-=======
-"""
-hello -A
-"""
-holi - edurne
->>>>>>> 440e562380cd1962d145ee5d8eceb06bea663f5f
